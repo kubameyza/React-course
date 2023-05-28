4046eb559c12c4c6c77d90a9b0cad8791d1efed5
Jakub Meyza <kubameyza@gmail.com>
2023-05-28 17:19:42 +0200

    Create ExpenseDate component

---
+++ Expense tracker/src/components/ExpenseDate/ExpenseDate.jsx
@@ -0,0 +1,16 @@
+import styles from './ExpensedDate.module.css';
+
+const ExpenseDate = ({ date }) => {
+  const month = date.toLocaleString('en-US', { month: 'long' });
+  const day = date.toLocaleString('en-US', { day: '2-digit' });
+  const year = date.getFullYear();
+
+  return (
+    <div className={styles['expense-date']}>
+      <div className={styles['expense-date__month']}>{month}</div>
+      <div className={styles['expense-date__year']}>{year} </div>
+      <div className={styles['expense-date__day']}>{day} </div>
+    </div>
+  );
+};
+export default ExpenseDate;
